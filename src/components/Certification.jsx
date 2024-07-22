04ab5ab39a759a2fafbd29e50de3ff48bfde8de9
Salman Shaikh <salmanshaikh18786@gmail.com>
2024-07-23 02:09:31 +0530

    update certification

--- src/components/Certification.jsx
+++ src/components/Certification.jsx
@@ -15,7 +15,7 @@ const Certification = () => {
           certification details are as follows.
         </p>
       </div>
-      <div className="h-full w-full flex justify-center items-center my-10">
+      <div className="h-full w-full flex flex-col gap-10 justify-center items-center my-10">
         <div className="__educationCard bg-[#171721] h-[30%] md:w-[60%] w-full mx-2 sm:mx-0 p-4 rounded-xl">
           <div className="flex flex-cols items-center gap-4">
             <div className="sm:h-14 h-12 bg-white">
@@ -35,7 +35,7 @@ const Certification = () => {
               <p className="text-zinc-400 text-sm">2023-2024</p>
             </div>
           </div>
-          <h1 className="text-zinc-400 my-4 font-bold flex gap-4 justify-center items-start">
+          <h1 className="text-zinc-400 my-4 font-bold flex gap-4 justify-start items-start">
            <div className="text-lg">Skills: </div>
             <div className="flex flex-wrap justify-start font-medium items-center gap-2">
               <p className="text-[#9254FD] bg-[#1D1E3A] py-1 px-2 rounded-full text-[15px]">
@@ -73,6 +73,61 @@ const Certification = () => {
             <a
               target="_blank"
               href="https://pwskills.com/learn/certificate/146f7e60-6177-4d65-a050-780f4931adbe/"
+              className="px-4 py-2 transition-all ease-in-out duration-300 hover:bg-[#570b9e] hover:scale-105 bg-[#6615b1] rounded-xl text-[15px]"
+            >
+              View Certificate
+            </a>
+          </div>
+        </div>
+        <div className="__educationCard bg-[#171721] h-[30%] md:w-[60%] w-full mx-2 sm:mx-0 p-4 rounded-xl">
+          <div className="flex flex-cols items-center gap-4">
+            <div className="sm:h-14 h-12 bg-white">
+              <img
+                src="https://accounts.pwskills.com/images/PWSkills-main.png"
+                className="h-full w-full rounded-full"
+                alt=""
+              />
+            </div>
+            <div>
+              <p className="text-lg md:text-lg font-bold text-zinc-400">
+                PW Skills
+              </p>
+              <p className="text-zinc-400 text-[16px] md:font-bold">
+                Backend Development
+              </p>
+              <p className="text-zinc-400 text-sm">2024</p>
+            </div>
+          </div>
+          <h1 className="text-zinc-400 my-4 font-bold flex gap-4 justify-start items-start">
+           <div className="text-lg">Skills: </div>
+            <div className="flex flex-wrap justify-start font-medium items-center gap-2">
+            
+              <p className="text-[#9254FD] bg-[#1D1E3A] py-1 px-2 rounded-full text-[15px]">
+                JavaSript
+              </p>
+              <p className="text-[#9254FD] bg-[#1D1E3A] py-1 px-2 rounded-full text-[15px]">
+                NodeJs
+              </p>
+              <p className="text-[#9254FD] bg-[#1D1E3A] py-1 px-2 rounded-full text-[15px]">
+                ExpressJS
+              </p>
+              <p className="text-[#9254FD] bg-[#1D1E3A] py-1 px-2 rounded-full text-[15px]">
+                MongoDB
+              </p>
+              <p className="text-[#9254FD] bg-[#1D1E3A] py-1 px-2 rounded-full text-[15px]">
+                REST APIs
+              </p>
+            </div>
+          </h1>
+          <div className="flex flex-col justify-center items-start gap-4">
+            <p className="text-zinc-400 text-[15px]">
+              I certified in Backend Development from{" "}
+              <span>pwskill.com</span>. This certification highlights my ability
+              to create comprehensive web applications.
+            </p>
+            <a
+              target="_blank"
+              href="https://pwskills.com/learn/certificate/60adc4b8-82ef-444d-94f7-a90164706de2/"
               className="px-4 py-2 transition-all ease-in-out duration-300 hover:bg-[#570b9e] hover:scale-105 bg-[#6615b1] rounded-xl text-[15px]"
             >
               View Certificate
